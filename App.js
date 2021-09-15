497ad298c4bc5739c53207b43c095e59546371ca
hwayoung baek <hybeak0@gmail.com>
2021-09-15 22:35:48 +0900

    load grid view image npm

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Testing the app</Text>
-      <StatusBar style="auto" />
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import GridImageView from 'react-native-grid-image-viewer';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+       <View style={styles.background}>
+      <Text style={styles.headline_text}>Grid View Images</Text>
+      <Text style={styles.explore_text}>Click on an image to view in full screen mode</Text>
+      
+      {/* Basic Usage */}
+      <GridImageView data={[ 'favicon.png', 'favicon.png', 'favicon.png', 'favicon.png' ]} />
     </View>
-  );
-}
+    );
+  }
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  background: {
+    backgroundColor: 'black',
+    flex: 1
+  },
+  headline_text: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 50,
+    marginLeft: 20
+  },
+  explore_text: {
+    marginTop: 5,
+    marginBottom: 10,
+    color: 'white',
+    marginLeft: 20,
+    fontSize: 12,
+    fontWeight: '600'
   },
 });
